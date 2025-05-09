ea85ec5c4b1990f7866a03336857c5e08ea0381f
theolukayodeasemudara <blaqnativity@gmail.com>
2025-05-09 08:02:36 +0100

    commited a quick minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
